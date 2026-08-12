237cda50cc908b75b816ecc864d9ad9fafdd06f6
Priya <preeyaahh26@gmail.com>
2026-08-12 12:23:16 +0530

    Complete job portal backend and resume features

--- src/main/java/com/jobportal/jobportal/repository/ApplicationRepository.java
+++ src/main/java/com/jobportal/jobportal/repository/ApplicationRepository.java
@@ -5,9 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface ApplicationRepository extends JpaRepository<Application, Long> {
+public interface ApplicationRepository
+        extends JpaRepository<Application, Long> {
 
-    List<Application> findByApplicantEmail(String applicantEmail);
+    List<Application> findByApplicantEmail(
+            String applicantEmail
+    );
 
     List<Application> findByJobId(Long jobId);
 }
